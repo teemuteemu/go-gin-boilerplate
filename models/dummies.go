8e0c432f14157e4655951712cb20ed9c8ee8324d
Teemu Kallio <teemu.kallio@dka.io>
2020-02-17 12:56:15 +0100

    JSON for the models

--- models/dummies.go
+++ models/dummies.go
@@ -1,10 +1,13 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 type Dummy struct {
-	gorm.Model
-	SomeField string `json:"some_field"`
+	ID        uint       `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+	SomeField string     `json:"some_field"`
 }
